01986f7ec5ac56bcf27344981ec8174b49ce50b6
eveem <monpriya.tammavong@gmail.com>
2017-03-29 23:59:54 +0700

    Add save image command

--- s_command.py
+++ s_command.py
@@ -1,9 +1,12 @@
 import numpy as numpy
 import cv2
 
-img = cv2.imread ('./image/cat.jpg', 0) # 0 is grayscale, 1 is original	
+img = cv2.imread ('./image/cat.jpg', 0) 		# 0 is grayscale, 1 is original	
 
 cv2.namedWindow ('image', cv2.WINDOW_NORMAL)	# can resize window
+
+cv2.imwrite ('./image/cat_gray.jpg', img)		# save to new image
+
 cv2.imshow ('image', img)
 cv2.waitKey (0)
 cv2.destroyAllWindows ()
