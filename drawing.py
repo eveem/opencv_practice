485412b3a66ece14d3d4e8407b4c9bad227a033b
eveem <monpriya.tammavong@gmail.com>
2017-03-30 12:46:27 +0700

    Add Text

--- drawing.py
+++ drawing.py
@@ -3,10 +3,13 @@ import cv2
 
 img = np.zeros ((512, 512, 3), np.uint8)
 
-# img = cv2.line (img, (0, 0), (511, 511), (250, 0, 0), 5)			 # draw line
-# img = cv2.rectangle (img, (384, 0), (510, 128), (0, 255, 0), 3)	 # draw rectangle
-# img = cv2.circle (img, (447, 63), 63, (0, 0, 255), -1)			 # draw circle
-# img = cv2.ellipse (img, (256, 256), (100, 50), 0, 0, 180, 255, -1) # draw ellipse
+img = cv2.line (img, (0, 0), (511, 511), (250, 0, 0), 5)			 # draw line
+img = cv2.rectangle (img, (384, 0), (510, 128), (0, 255, 0), 3)	 	 # draw rectangle
+img = cv2.circle (img, (447, 63), 63, (0, 0, 255), -1)			 	 # draw circle
+img = cv2.ellipse (img, (256, 256), (100, 50), 0, 0, 180, 255, -1) 	 # draw ellipse
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+cv2.putText (img, 'Eve', (10, 500), font, 4, (255, 255, 255), 2, cv2.LINE_AA)
 
 cv2.imshow ('image', img)
 cv2.waitKey (0)
